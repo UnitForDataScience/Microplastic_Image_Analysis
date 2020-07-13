a968bf5e01039d707109a69bb564aa5b44bb556d
djlittle <42420410+djlittle@users.noreply.github.com>
2020-07-13 11:46:50 -0700

    Initial commit

---
+++ base.py
@@ -0,0 +1,13 @@
+# -------------------------------------------------------------------------------
+# Name: base.py
+# Purpose: Base code to begin working with Microplastics data
+#
+# Author(s):    David Little
+#
+# Created:      07/13/2020
+# Updated:
+# Update Comment(s):
+#
+# TO DO:
+#
+# -------------------------------------------------------------------------------
